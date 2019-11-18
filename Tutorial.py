80b7e0d8b3cecc8005ede23c22b234ab25aae06b
vicennao000 <42577046+vicennao000@users.noreply.github.com>
2019-11-18 10:19:13 -0600

    Update Tutorial.py

--- Tutorial.py
+++ Tutorial.py
@@ -24,6 +24,11 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
 var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
 
 var polygon = L.polygon([
     [51.509, -0.08],
@@ -49,7 +54,7 @@ mymap.on('click', onMapClick);
 var popup = L.popup();
 
 function onMapClick(e) {
-   popup
+    popup
         .setLatLng(e.latlng)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(mymap);
